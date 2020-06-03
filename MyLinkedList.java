5aa11ccb8bdf9b22f90db24773a06b6120d1f2bd
coderInPractice <coderInPractice@noreply.github.com>
2020-06-04 01:36:52 +0530

    Create MyLinkedList.java
    Single Linked List implementation. It has following methods:
    1. Add Node (Tail)
    2. Print Nodes
    3. Remove Node(Tail)
    4.Peek(For Stack Purpose)

---
+++ MyLinkedList.java
@@ -0,0 +1,81 @@
+package data_structure;
+
+public class MyLinkedList {
+	
+	Node head;
+	
+	public class  Node{
+		int data;
+		Node next;
+		
+		public Node(int data) {
+			this.data = data;
+			next = null;
+		}
+	}
+	
+	// Add new node at tail
+	public void addNode(int data) {
+		Node newNode =  new Node (data);
+		
+		if(head == null) {
+			head = newNode;
+			return;
+		}
+		
+		Node currentNode = head;
+		
+		while(currentNode.next != null) {
+			currentNode = currentNode.next;
+		}
+		
+		currentNode.next = newNode;
+	}
+	
+	// print
+	
+	void printLL () {
+		Node current = head;
+		
+		while(current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+	
+	
+	//remove from tail
+	
+	int removeTail() {
+		Node current = head;
+		
+		if(current == null) {
+			System.out.println("List is already empty");
+		}
+		
+		if(current.next == null) {
+			current = null;
+			System.out.println("Head is deleted");
+		}
+		
+		while(current.next.next != null) {
+			current = current.next;
+		}
+		Node toremove = current;
+		current.next = null;
+		
+		return toremove.data;
+	}
+	
+	public int peek() {
+		
+		Node current  = head;
+		while(current.next != null) {
+			current = current.next;
+		}
+		
+		return current.data;
+	}
+	}
+
